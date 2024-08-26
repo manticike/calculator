73608bc788a730ea8d5ec7e4670d7b1cf577486e
manticike <antwikisaac@gmail.com>
2024-08-26 05:42:00 +0000

    Refactored JavaScript Code. I've refactored the javscript code all over again

--- calculator.js
+++ calculator.js
@@ -1,9 +1,16 @@
+let firstOperand = '';
+let secondOperand = '';
+let currentOperator = null;
+let waitingForSecondOperand = false;
 
-const display = document.querySelector('#display');
-const userInputScreen = document.querySelector('#user-input');
-const answerScreen = document.querySelector('#answer');
-let numbersButton = document.querySelectorAll('.numbers');
-
+const display = document.querySelector('#user-input');
+const result = document.querySelector('#result');
+const deleteButton = document.querySelector('#delete');
+const clearScreenButton = document.querySelector('#clear');
+const numbersButton = document.querySelectorAll('.numbers');
+const operatorsButton = document.querySelectorAll('.operators');
+const equalsButton = document.querySelector('#equals');
+const point = document.querySelector('#point');
 
 function add(num1, num2) {
   return num1 + num2;
@@ -18,27 +25,94 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-  if (num2 !== 0) {
-    return num1/num2;
-  }else {
-    return "Error"
+  if (num2 === 0) {
+    return 'ERROR';
+  } else {
+    return num1 / num2;
   }
 }
 
+//function percentage(num1,)
 
 function operate(num1, num2, operator) {
+  firstOperand = parseFloat(num1);
+  secondOperand = parseFloat(num2);
+
   switch(operator) {
     case '+':
-      add(num1, num2);
-      break;
+      return add(firstOperand, secondOperand);
     case '-':
-      subtract(num1, num2);
-      break;
+      return subtract(firstOperand, secondOperand);
     case '*':
-      multiply(num1, num2);
-      break;
+      return multiply(firstOperand, secondOperand);
     case '/':
-      divide(num1, num2);
-      break;
+      return divide(firstOperand, secondOperand);
+    default:
+      return 'ERROR';
   }
 }
+
+function populateScreen(value) {
+ if (!waitingForSecondOperand) {
+  if (firstOperand === '0' || firstOperand == '') {
+    firstOperand = value;
+  } else {
+    firstOperand += value;
+  }
+  display.textContent = firstOperand + ' ' + (currentOperator || ' ');
+ } else {
+  if (secondOperand === '0' || secondOperand === '') {
+    secondOperand = value;
+  } else {
+    secondOperand += value;
+  }
+  display.textContent = firstOperand + ' ' + currentOperator + ' ' + secondOperand;
+ }
+}
+
+
+numbersButton.forEach(button => {
+  button.addEventListener('click',function () {
+    populateScreen(button.value);
+  });
+});
+
+operatorsButton.forEach(button => {
+  button.addEventListener('click', function(value) {
+    if (currentOperator && !waitingForSecondOperand) {
+      currentOperator = button.value;
+      display.textContent = firstOperand + ' ' + currentOperator;
+    } else if (firstOperand !== '') {
+      currentOperator = button.value;
+      waitingForSecondOperand = true;
+      display.textContent = firstOperand + ' ' + currentOperator;
+    }
+  });
+})
+
+
+equalsButton.addEventListener('click', () => {
+  answer = operate(firstOperand, secondOperand, currentOperator);
+  result.textContent = answer;
+});
+
+
+function deleteNumber() {
+  if (waitingForSecondOperand && secondOperand !== '') {
+    secondOperand = secondOperand.slice(0, -1);
+    display.textContent = firstOperand + ' ' + currentOperator + ' ' + secondOperand;
+  } else if (!waitingForSecondOperand && currentOperator !== '') {
+   currentOperator = '';
+   display.textContent = firstOperand;
+
+  } else if (!waitingForSecondOperand && firstOperand !== '') {
+    firstOperand = firstOperand.slice(0, -1);
+    display.textContent = firstOperand;
+  }
+}
+
+clearScreenButton.addEventListener('click', function() {
+  location.reload();
+});
+
+deleteButton.addEventListener('click', deleteNumber);
